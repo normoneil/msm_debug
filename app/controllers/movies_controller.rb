ce59c5f9d69b24aabb671ede72bbeb578d1db0fe
normoneil <normoneil@users.noreply.github.com>
2016-11-07 14:12:59 -0600

    2 Failures remaining, 96%

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -29,12 +29,14 @@ class MoviesController < ApplicationController
 
   def update_row
     @movie = Movie.find(params[:id])
+
     @movie.title = params[:title]
     @movie.year = params[:year]
     @movie.duration = params[:duration]
     @movie.description = params[:description]
     @movie.image_url = params[:image_url]
     @movie.director_id = params[:director_id]
+    
     @movie.save
 
     render("show")
